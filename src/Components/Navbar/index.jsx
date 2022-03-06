c4f2f40e56a72bcb755908b92b8835dbd45b1986
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-03-06 03:18:49 -0300

    fix navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -4,7 +4,7 @@ import { NavbarContainer, Title } from "./style.js";
 export default function Navbar() {
   return (
     <NavbarContainer>
-      <Title>Home</Title>
+      <Title>Home!</Title>
       <Title>Todas as Receitas</Title>
       <Title>Quem sou eu</Title>
     </NavbarContainer>
